7913d49ab423702d176b25f26d8e08200b45dfd3
Frank <frankkashere@gmail.com>
2023-10-08 01:31:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity){
+    if(activity){
+        return `This Saturday, I want to ${activity}!`
+    }
+    else if(" "){
+        return `This Saturday, I want to roller-skate!`
+    }
+    
+}
+
+const mondayWork = function(work){
+    if(work){
+        return `This Monday, I will ${work}.`
+    }
+    else if(" "){
+        return `This Monday, I will go to the office.`
+    }
+}
+function wrapAdjective(input="*"){
+    return function(par="special"){
+        return `You are ${input}${par}${input}!`
+    }
+}
